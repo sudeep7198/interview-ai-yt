632f7ef4d07948462f9ab11c8a4941ca30e544f4
Sudeep Kumar <sudeep1234@gmail.com>
2026-05-20 22:32:03 +0530

    added jenkinsfile

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,9 @@ const connectToDB = require("./src/config/database")
 
 connectToDB()
 
+app.get("/", (req, res) => {
+    res.send("Backend Running Successfully")
+})
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
